2c9126b05f5a7f7799dae09299bfd2bb8bc408c7
Daniel Soto <dasg.1993@gmail.com>
2022-01-24 12:24:47 -0500

    [feature] - add log out in maijn activity & implemente sign out

--- app/src/main/java/com/danielsoto/idatcourse/MainActivity.java
+++ app/src/main/java/com/danielsoto/idatcourse/MainActivity.java
@@ -2,13 +2,31 @@ package com.danielsoto.idatcourse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button logoutButton;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logoutButton = findViewById(R.id.logout_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
